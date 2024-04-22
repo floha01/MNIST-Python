80dab9b2b2a7738b0c2c2aedc982891a0b1367ef
floha01 <flo.hartl@gmx.net>
2024-04-22 17:06:52 +0200

    Finished prediction function

--- model.py
+++ model.py
@@ -46,6 +46,10 @@ model.add(tf.keras.layers.Dense(10, activation="softmax"))
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train, epochs=3)
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(loss)
+print(accuracy)
 
 model.save("mnist.keras")
--- predict.py
+++ predict.py
@@ -1,3 +1,23 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
+import os
+import cv2
+import numpy as np
 
+# Load Model
 model = tf.keras.models.load_model("mnist.keras")
+
+# Load test images
+test_folder = "test-img"
+file_list = os.listdir(test_folder)
+
+for file_name in file_list:
+    image_path = os.path.join(test_folder, file_name)
+    image = cv2.imread(image_path)[:, :, 0]
+    image = np.invert(image)
+    image = np.array([image])
+    prediction = model.predict(image)
+    pred_number = np.argmax(prediction)
+    plt.imshow(image[0], cmap="binary")
+    plt.title(f"This is probably a {pred_number}")
+    plt.show()
