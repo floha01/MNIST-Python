83ea446c6412392fc2e47e644ea61d63b0a4f7dd
floha01 <flo.hartl@gmx.net>
2024-04-22 21:11:28 +0200

    Added try and catch

--- predict.py
+++ predict.py
@@ -13,11 +13,18 @@ file_list = os.listdir(test_folder)
 
 for file_name in file_list:
     image_path = os.path.join(test_folder, file_name)
-    image = cv2.imread(image_path)[:, :, 0]
-    image = np.invert(image)
-    image = np.array([image])
-    prediction = model.predict(image)
-    pred_number = np.argmax(prediction)
-    plt.imshow(image[0], cmap="binary")
-    plt.title(f"This is probably a {pred_number}")
-    plt.show()
+    try:
+        image = cv2.imread(image_path)
+        if image is not None:
+            image = image[:, :, 0]
+            image = np.invert(image)
+            image = np.array([image])
+            prediction = model.predict(image)
+            pred_number = np.argmax(prediction)
+            plt.imshow(image[0], cmap="binary")
+            plt.title(f"This is probably a {pred_number}")
+            plt.show()
+        else:
+            print(f"Error: Image could not be loaded {image_path}")
+    except Exception as e:
+        print(f"Error during prcessing of {image_path}: {str(e)}")
