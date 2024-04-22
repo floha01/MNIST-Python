243d65a67c4020d808a802605c578b1129a7a0a3
floha01 <flo.hartl@gmx.net>
2024-04-22 16:15:29 +0200

    Load dataset and find out the test ratio

--- model.py
+++ model.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+import os
 
+# Load Data
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Find out the split ratio
+print("x_train " + str(x_train.shape))
+print("y_train " + str(y_train.shape))
+print("x_test " + str(x_test.shape))
+print("y_test " + str(y_test.shape))
