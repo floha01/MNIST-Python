c74b760672dae145f743aaf96164f55025eaeb23
floha01 <flo.hartl@gmx.net>
2024-04-22 16:33:25 +0200

    Visualize the data

--- model.py
+++ model.py
@@ -14,3 +14,18 @@ print("x_train " + str(x_train.shape))
 print("y_train " + str(y_train.shape))
 print("x_test " + str(x_test.shape))
 print("y_test " + str(y_test.shape))
+
+# Plotting the data
+num_images = 3
+
+# Show one Image at a time
+
+for i in range(num_images):
+    plt.imshow(x_train[i], cmap="grey")
+    plt.show()
+
+# Show all images in one plot
+for i in range(num_images):
+    plt.subplot(330 + 1 + i)
+    plt.imshow(x_train[i], cmap="gray")
+plt.show()
